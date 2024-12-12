f6374b2a7d274617bf984a5668ecc14124fb2931
Ghaitsa Zahira Rahma <101691351+ghaitsazahirar@users.noreply.github.com>
2024-12-12 12:08:55 +0700

    Update app.js
    GANTI APP.JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 import './bootstrap';
+import 'flowbite';
+import 'tailwindcss';
+
+document.getElementById('dropdownNavbarLink').addEventListener('click', function(event) {
+    const dropdown = document.getElementById('dropdownNavbar');
+    if (dropdown.classList.contains('hidden') === false) {
+        event.preventDefault(); // Mencegah redirect jika dropdown aktif
+    }
+});
+
+const accButton = document.getElementById("acc");
+        const tolakButton = document.getElementById("tolak");
+        const formulirPengajuan = document.getElementById("formulirPengajuan");
+
+        accButton.addEventListener("change", () => {
+            if (accButton.checked) {
+                formulirPengajuan.classList.remove("hidden");
+            }
+        });
+
+        tolakButton.addEventListener("change", () => {
+            if (tolakButton.checked) {
+                formulirPengajuan.classList.add("hidden");
+            }
+        });
